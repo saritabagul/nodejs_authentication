03a039d10d7f68c6f0fd4004584cc24b152067bc
sarita bagul <34671228+saritabagul@users.noreply.github.com>
2023-07-21 11:47:28 +0530

    Update passport-google-oauth2-strategy.js

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -5,13 +5,9 @@ const User = require('../models/User');
 
 //tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
-    // clientID:"****", // put your account clientID
-    // clientSecret:"*****", // put your account clientSecret
-    // callbackURL:"*****", // put your account callbackURL
-
-    clientID:"843140863994-psp173sd7k4svv1i3h4oeu6us6al1ot1.apps.googleusercontent.com",
-    clientSecret:"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",
-    callbackURL:"https://tame-jade-ostrich-kilt.cyclic.app/auth/google/callback",
+    clientID:"****", // put your account clientID
+    clientSecret:"*****", // put your account clientSecret
+    callbackURL:"*****", // put your account callbackURL    
 },
 async function(accessToken,refreshToken,profile,done){
     try{
